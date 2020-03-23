2fcb895d3507d1c0432df2b249cdb4cf4a472070
Carol (Nichols || Goulding) <carol.nichols@integer32.com>
2020-03-23 13:08:16 -0400

    test: Deduplicate proptest input streams to match real input
    The input streams can be assumed to not contain duplicates.

--- src/storage/partitioned_store.rs
+++ src/storage/partitioned_store.rs
@@ -533,7 +533,7 @@ mod tests {
 
     /// Given a vector of optional values, put all the `Some` values in sorted order, leaving the
     /// `None` values interspersed as they are.
-    fn sort_present_values<T: Ord>(d: &mut Vec<Option<T>>) {
+    fn sort_and_dedup_present_values<T: Ord>(d: &mut Vec<Option<T>>) {
         let mut values = vec![];
         let mut indices = vec![];
 
@@ -546,6 +546,11 @@ mod tests {
 
         values.sort();
 
+        // Remove duplicates because this is an invariant upheld by the input streams. If this
+        // assumption changes, this code should be updated.
+        // Duplicates will become `None` values.
+        values.dedup();
+
         for (v, i) in values.into_iter().zip(indices) {
             d[i] = Some(v);
         }
@@ -556,7 +561,7 @@ mod tests {
     fn arb_proto_stream<T: Arbitrary + Ord + Clone>() -> impl Strategy<Value = ProtoStream<T>> {
         // Generate a vector of optional values
         any::<Vec<Option<T>>>().prop_map(|mut v| {
-            sort_present_values(&mut v);
+            sort_and_dedup_present_values(&mut v);
 
             // `original` will contain the sorted present values only
             let original = v.clone().into_iter().flatten().collect();
