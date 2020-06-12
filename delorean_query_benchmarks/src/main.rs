b8d50e331839e36e3c5e476c25f5b94b349285e7
Paul Dix <paul@pauldix.net>
2020-06-11 20:03:58 -0400

    Update query benchmark main.rs
    Updated query benchmark to create the same dataset as those used in benches along with a timer for how long the query took.

--- delorean_query_benchmarks/src/main.rs
+++ delorean_query_benchmarks/src/main.rs
@@ -1,23 +1,41 @@
 use arrow::util::pretty;
 use delorean_query_benchmarks::{generate_test_data, query, Column};
 
-const NUM_BATCHES: usize = 1;
-const NUM_RECORDS: usize = 1000;
-
 fn main() {
     println!("generating test data");
-    let columns = vec![
-        Column::new_i64("i64".to_string(), None, true),
-        Column::new_f64("f64".to_string(), None, true),
-        Column::new_i64("ts".to_string(), None, true),
-        Column::new_utf8("tag".to_string(), None, true),
-    ];
-    let (schema, data) = generate_test_data(NUM_BATCHES, NUM_RECORDS, columns);
+    let batch_count = 1;
+    let record_count = 1000000;
+    let total_records = batch_count * record_count;
+    let mut columns = vec![];
 
-    let query_str = "select i64, ts, count(ts), min(i64) from foo where ts > 0 group by i64, ts";
-    println!("running query: {}", query_str);
+    for i in 1..50 as usize {
+        let cardinality = i.pow(5);
 
+        columns.push(Column::new_i64(
+            format!("i64_{}", i).to_string(),
+            Some(cardinality),
+            false,
+        ));
+    }
+
+    for i in 1..51 {
+        columns.push(Column::new_f64(
+            format!("f64_{}", i).to_string(),
+            None,
+            false,
+        ))
+    }
+
+    columns.push(Column::new_i64("ts".to_string(), None, false));
+
+    let (schema, data) = generate_test_data(batch_count, record_count, columns);
+
+    let query_str = "select sum(i64_2) from foo where ts > 99 and i64_49 = 12345";
+    println!("running query on {} records: {}", total_records, query_str);
+
+    let now = std::time::Instant::now();
     let results = query(query_str, "foo", schema, data).unwrap();
+    println!("ran query in {}.{} seconds", now.elapsed().as_secs(), now.elapsed().subsec_millis());
 
     pretty::print_batches(&results).unwrap();
 }
