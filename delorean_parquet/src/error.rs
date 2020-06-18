b521771b3be799c85b6ee8d790b111557f4d0a69
Andrew Lamb <alamb@influxdata.com>
2020-06-18 18:25:54 -0400

    fix: Update delorean_parquet/src/error.rs
    Co-authored-by: Carol (Nichols || Goulding) <193874+carols10cents@users.noreply.github.com>

--- delorean_parquet/src/error.rs
+++ delorean_parquet/src/error.rs
@@ -4,7 +4,7 @@ use parquet::errors::ParquetError;
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display(r#"{}, underlying parqet error {}"#, message, source))]
+    #[snafu(display(r#"{}, underlying parquet error {}"#, message, source))]
     ParquetLibraryError {
         message: String,
         source: ParquetError,
