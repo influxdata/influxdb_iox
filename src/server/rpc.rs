b84b07946a330c91f22bc7e50521a53c296b4e49
Andrew Lamb <alamb@influxdata.com>
2020-12-02 08:24:05 -0500

    docs: Update src/server/rpc.rs

--- src/server/rpc.rs
+++ src/server/rpc.rs
@@ -5,7 +5,7 @@
 pub(crate) const TAG_KEY_MEASUREMENT: &[u8] = &[0];
 
 /// `[0xff]` is is the magic value that that the storage gRPC layer uses
-/// to encode a tag_key that means "measurement name"
+/// to encode a tag_key that means "field name"
 pub(crate) const TAG_KEY_FIELD: &[u8] = &[255];
 
 pub mod data;
