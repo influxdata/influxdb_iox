f63abf08b009822ef27b9d19a36d10a7ec6eff1c
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2021-03-05 16:49:52 +0000

    feat: add fanout task tracking

--- data_types/src/lib.rs
+++ data_types/src/lib.rs
@@ -10,6 +10,7 @@
     clippy::clone_on_ref_ptr
 )]
 
+pub use database_name::*;
 pub use schema::TIME_COLUMN_NAME;
 
 /// The name of the column containing table names returned by a call to
@@ -24,6 +25,7 @@ pub mod data;
 pub mod database_rules;
 pub mod error;
 pub mod http;
+pub mod job;
 pub mod names;
 pub mod partition_metadata;
 pub mod schema;
@@ -32,6 +34,4 @@ pub mod timestamp;
 pub mod wal;
 
 mod database_name;
-pub use database_name::*;
-
 pub(crate) mod field_validation;
