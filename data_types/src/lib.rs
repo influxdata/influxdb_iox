286211f97c6f8e6049e589705c2ff60213d74578
Andrew Lamb <andrew@nerdnetworks.org>
2021-03-18 13:21:54 -0400

    refactor: remove some stragglers

--- data_types/src/lib.rs
+++ data_types/src/lib.rs
@@ -12,17 +12,6 @@
 
 pub use database_name::*;
 
-/// The name of the timestamp column
-//pub const TIME_COLUMN_NAME: &str = "time";
-
-/// The name of the column containing table names returned by a call to
-/// `table_names`.
-pub const TABLE_NAMES_COLUMN_NAME: &str = "table";
-
-/// The name of the column containing column names returned by a call to
-/// `column_names`.
-pub const COLUMN_NAMES_COLUMN_NAME: &str = "column";
-
 pub mod chunk;
 pub mod database_rules;
 pub mod error;
