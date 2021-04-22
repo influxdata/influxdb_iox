d8cda34df52c7332feceb5529c82bdb8e9a4fa57
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2021-04-22 20:49:12 +0100

    feat: string dictionary encoding (#1220)

--- internal_types/src/schema.rs
+++ internal_types/src/schema.rs
@@ -592,11 +592,19 @@ pub enum InfluxColumnType {
 impl InfluxColumnType {
     /// returns true if `arrow_type` can validly store this column type
     pub fn valid_arrow_type(&self, data_type: &ArrowDataType) -> bool {
-        // Note this function is forward looking and imagines the day
-        // when types like `Tag` can be stored as Utf8 or various
-        // StringDictionary types.
-        let default_type: ArrowDataType = self.into();
-        data_type == &default_type
+        match self {
+            Self::Tag => match data_type {
+                ArrowDataType::Utf8 => true,
+                ArrowDataType::Dictionary(key, value) => {
+                    key.as_ref() == &ArrowDataType::Int32 && value.as_ref() == &ArrowDataType::Utf8
+                }
+                _ => false,
+            },
+            Self::Field(_) | Self::Timestamp => {
+                let default_type: ArrowDataType = self.into();
+                data_type == &default_type
+            }
+        }
     }
 }
 
