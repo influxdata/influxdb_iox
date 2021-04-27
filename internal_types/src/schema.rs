1025efa80e276cd2127beed913f9efff700bbab5
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2021-04-27 10:31:31 +0100

    chore: fix lint

--- internal_types/src/schema.rs
+++ internal_types/src/schema.rs
@@ -654,10 +654,7 @@ impl From<&InfluxColumnType> for ArrowDataType {
     /// What arrow type is used for this column type?
     fn from(t: &InfluxColumnType) -> Self {
         match t {
-            InfluxColumnType::Tag => ArrowDataType::Dictionary(
-                Box::new(ArrowDataType::Int32),
-                Box::new(ArrowDataType::Utf8),
-            ),
+            InfluxColumnType::Tag => Self::Dictionary(Box::new(Self::Int32), Box::new(Self::Utf8)),
             InfluxColumnType::Field(influxdb_field_type) => (*influxdb_field_type).into(),
             InfluxColumnType::Timestamp => TIME_DATA_TYPE(),
         }
