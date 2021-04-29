da111ec9be44fb37e55b0544c32249a5aa46b782
Andrew Lamb <andrew@nerdnetworks.org>
2021-04-29 11:38:46 -0400

    fix: clippy

--- tests/end_to_end_cases/scenario.rs
+++ tests/end_to_end_cases/scenario.rs
@@ -17,8 +17,7 @@ use data_types::{names::org_and_bucket_to_database, DatabaseName};
 use generated_types::{influxdata::iox::management::v1::DatabaseRules, ReadSource, TimestampRange};
 
 use arrow_deps::arrow::{
-    array::{Array, ArrayRef, Float64Array, StringArray, TimestampNanosecondArray},
-    datatypes::{Field, Schema},
+    array::{ArrayRef, Float64Array, StringArray, TimestampNanosecondArray},
     record_batch::RecordBatch,
 };
 
