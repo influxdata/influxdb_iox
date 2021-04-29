063c9236906c26d8e00df90157beea8792f8cbb4
Andrew Lamb <andrew@nerdnetworks.org>
2021-04-29 12:13:48 -0400

    feat: add a id() method to Operations management client

--- generated_types/src/google.rs
+++ generated_types/src/google.rs
@@ -10,18 +10,16 @@ pub mod rpc {
 pub mod longrunning {
     include!(concat!(env!("OUT_DIR"), "/google.longrunning.rs"));
 
-
     impl Operation {
         /// Return the IOx operation `id`. This `id` can
         /// be passed to the various APIs in the
-        /// operations client [`wait_operation`];
+        /// operations client such as [`wait_operation`];
         pub fn id(&self) -> usize {
             self.name
                 .parse()
                 .expect("Internal error: id returned from server was not an integer")
         }
     }
-
 }
 
 use self::protobuf::Any;
