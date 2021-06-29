f67ea00e584d9fc327f35ca3e3fa072051d3fbe4
Andrew Lamb <andrew@nerdnetworks.org>
2021-06-29 09:35:20 -0400

    refactor: split setup and parser into separate modules

--- query_tests/src/runner.rs
+++ query_tests/src/runner.rs
@@ -1,30 +1,25 @@
 //! Basic test runner that runs queries in files and compares the output to the expected results
 
+mod parse;
+mod setup;
+
 use arrow::record_batch::RecordBatch;
 use query::frontend::sql::SqlQueryPlanner;
-use snafu::{OptionExt, ResultExt, Snafu};
+use snafu::{ResultExt, Snafu};
 use std::{
-    fmt::Display,
     io::BufWriter,
     io::Write,
     path::{Path, PathBuf},
     sync::Arc,
 };
 
-use crate::scenarios::{get_all_setups, get_db_setup, DbScenario, DbSetup};
-
-const IOX_SETUP_NEEDLE: &str = "-- IOX_SETUP: ";
+use self::{parse::TestQueries, setup::TestSetup};
+use crate::scenarios::{DbScenario, DbSetup};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
     #[snafu(display("Can not find case file '{:?}': {}", path, source))]
     NoCaseFile {
-        path: PathBuf,
-        source: std::io::Error,
-    },
-
-    #[snafu(display("Can not find expected output file '{:?}': {}", path, source))]
-    NoExpectedFile {
         path: PathBuf,
         source: std::io::Error,
     },
@@ -54,12 +49,6 @@ pub enum Error {
     },
 
     #[snafu(display(
-        "No setup found. Looking for lines that start with '{}'",
-        IOX_SETUP_NEEDLE
-    ))]
-    SetupNotFound {},
-
-    #[snafu(display(
         "Contents of output '{:?}' does not match contents of expected '{:?}'",
         output_path,
         expected_path
@@ -69,22 +58,8 @@ pub enum Error {
         expected_path: PathBuf,
     },
 
-    #[snafu(display("No setup named '{}' found. Perhaps you need to define it in scenarios.rs. Known setups: {:?}",
-                    setup_name, known_setups))]
-    NamedSetupNotFound {
-        setup_name: String,
-        known_setups: Vec<String>,
-    },
-
-    #[snafu(display(
-        "Only one setup is supported. Previously say setup '{}' and now saw '{}'",
-        setup_name,
-        new_setup_name
-    ))]
-    SecondSetupFound {
-        setup_name: String,
-        new_setup_name: String,
-    },
+    #[snafu(display("Test Setup Error: {}", source))]
+    SetupError { source: self::setup::Error },
 }
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -116,7 +91,7 @@ impl Runner {
             String::from_utf8(contents).context(CaseFileNotUtf8 { path: &input_path })?;
 
         println!("Processing contents:\n{}", contents);
-        let test_setup = TestSetup::try_from_lines(contents.lines())?;
+        let test_setup = TestSetup::try_from_lines(contents.lines()).context(SetupError)?;
         let queries = TestQueries::from_lines(contents.lines());
         println!("Using test setup:\n{}", test_setup);
 
@@ -128,7 +103,7 @@ impl Runner {
         let mut output = vec![];
         output.push(format!("-- Test Setup: {}", test_setup.setup_name()));
 
-        let db_setup = test_setup.get_setup()?;
+        let db_setup = test_setup.get_setup().context(SetupError)?;
         for q in queries.iter() {
             output.push(format!("-- SQL: {}", q));
 
@@ -229,7 +204,6 @@ impl Runner {
             }
             previous_results = current_results;
         }
-
         previous_results
     }
 }
@@ -242,234 +216,11 @@ fn make_absolute(path: &Path) -> PathBuf {
     absolute
 }
 
-/// Encapsulates the setup needed for a test
-///
-/// Currently supports the following commands
-///
-/// # Run the specified setup:
-/// # -- IOX_SETUP: SetupName
-#[derive(Debug, PartialEq)]
-struct TestSetup {
-    setup_name: String,
-}
-
-impl Display for TestSetup {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "TestSetup")?;
-        writeln!(f, "  Setup: {}", self.setup_name)?;
-
-        Ok(())
-    }
-}
-
-impl TestSetup {
-    fn setup_name(&self) -> &str {
-        &self.setup_name
-    }
-
-    fn get_setup(&self) -> Result<Arc<dyn DbSetup>> {
-        let setup_name = self.setup_name();
-        get_db_setup(setup_name).with_context(|| {
-            let known_setups = get_all_setups()
-                .keys()
-                .map(|s| s.to_string())
-                .collect::<Vec<_>>();
-            NamedSetupNotFound {
-                setup_name,
-                known_setups,
-            }
-        })
-    }
-
-    /// Create a new TestSetup object from the lines
-    fn try_from_lines<I, S>(lines: I) -> Result<Self>
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<str>,
-    {
-        let mut parser = lines.into_iter().filter_map(|line| {
-            let line = line.as_ref().trim();
-            line.strip_prefix(IOX_SETUP_NEEDLE)
-                .map(|setup_name| setup_name.trim().to_string())
-        });
-
-        let setup_name = parser.next().context(SetupNotFound)?;
-
-        if let Some(new_setup_name) = parser.next() {
-            return SecondSetupFound {
-                setup_name,
-                new_setup_name,
-            }
-            .fail();
-        }
-
-        Ok(Self { setup_name })
-    }
-}
-
-// Poor man's parser to find all the SQL queries in the input
-#[derive(Debug, PartialEq)]
-struct TestQueries {
-    queries: Vec<String>,
-}
-
-impl TestQueries {
-    fn new<I, S>(queries: I) -> Self
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<str>,
-    {
-        let queries = queries
-            .into_iter()
-            .map(|s| s.as_ref().to_string())
-            .collect();
-
-        Self { queries }
-    }
-
-    /// find all queries (more or less a fancy split on `;`
-    fn from_lines<I, S>(lines: I) -> Self
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<str>,
-    {
-        let mut queries = vec![];
-        let mut current_line = String::new();
-
-        lines.into_iter().for_each(|line| {
-            let line = line.as_ref().trim();
-            if line.starts_with("--") {
-                return;
-            }
-            if line.is_empty() {
-                return;
-            }
-
-            // declare queries when we see a semicolon at the end of the line
-            if !current_line.is_empty() {
-                current_line.push(' ');
-            }
-            current_line.push_str(line);
-
-            if line.ends_with(';') {
-                // resets current_line to String::new()
-                let t = std::mem::take(&mut current_line);
-                queries.push(t);
-            }
-        });
-
-        if !current_line.is_empty() {
-            queries.push(current_line);
-        }
-
-        Self { queries }
-    }
-
-    // Get an iterator over the queries
-    fn iter(&self) -> impl Iterator<Item = &str> {
-        self.queries.iter().map(|s| s.as_str())
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test_parse_lines() {
-        let lines = vec!["Foo", "bar", "-- IOX_SETUP: MySetup", "goo"];
-        let setup = TestSetup::try_from_lines(lines).unwrap();
-        assert_eq!(
-            setup,
-            TestSetup {
-                setup_name: "MySetup".into()
-            }
-        );
-    }
-
-    #[test]
-    fn test_parse_lines_extra_whitespace() {
-        let lines = vec!["Foo", "  -- IOX_SETUP:   MySetup   "];
-        let setup = TestSetup::try_from_lines(lines).unwrap();
-        assert_eq!(
-            setup,
-            TestSetup {
-                setup_name: "MySetup".into()
-            }
-        );
-    }
-
-    #[test]
-    fn test_parse_lines_setup_name_with_whitespace() {
-        let lines = vec!["Foo", "  -- IOX_SETUP:   My Awesome  Setup   "];
-        let setup = TestSetup::try_from_lines(lines).unwrap();
-        assert_eq!(
-            setup,
-            TestSetup {
-                setup_name: "My Awesome  Setup".into()
-            }
-        );
-    }
-
-    #[test]
-    fn test_parse_lines_none() {
-        let lines = vec!["Foo", " MySetup   "];
-        let setup = TestSetup::try_from_lines(lines).unwrap_err().to_string();
-        assert_eq!(
-            setup,
-            "No setup found. Looking for lines that start with '-- IOX_SETUP: '"
-        );
-    }
-
-    #[test]
-    fn test_parse_lines_multi() {
-        let lines = vec!["Foo", "-- IOX_SETUP:   MySetup", "-- IOX_SETUP:   MySetup2"];
-        let setup = TestSetup::try_from_lines(lines).unwrap_err().to_string();
-        assert_eq!(
-            setup,
-            "Only one setup is supported. Previously say setup 'MySetup' and now saw 'MySetup2'"
-        );
-    }
-
-    #[test]
-    fn test_parse_queries() {
-        let input = r#"
--- This is a test
-select * from foo;
--- another comment
-
-select * from bar;
--- This query has been commented out and should not be seen
--- select * from baz;
-"#;
-        let queries = TestQueries::from_lines(input.split('\n'));
-        assert_eq!(
-            queries,
-            TestQueries::new(vec!["select * from foo;", "select * from bar;"])
-        )
-    }
-
-    #[test]
-    fn test_parse_queries_no_ending_semi() {
-        let input = r#"
-select * from foo;
--- no ending semi colon
-select * from bar
-"#;
-        let queries = TestQueries::from_lines(input.split('\n'));
-        assert_eq!(
-            queries,
-            TestQueries::new(vec!["select * from foo;", "select * from bar"])
-        )
-    }
-
-    #[test]
-    fn test_parse_queries_empty() {
-        let input = r#"
--- This is a test
--- another comment
-"#;
-        let queries = TestQueries::from_lines(input.split('\n'));
-        assert_eq!(queries, TestQueries::new(vec![] as Vec<String>))
-    }
+    // TODO; implement end to end test of the runner
+    #[tokio::test]
+    async fn runner_positive() {}
 }
